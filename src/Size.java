f5a6165ddb94b9f6256628489d25c70fd287da9f
Matei Munteanu <matei.munteanu@mta.ro>
2020-12-12 18:06:27 +0200

    Implementare size
    Issue:#7
    Am modificat clasa size astfel incat sa existe doar o limita maxima.
    
    Signed-off-by: Matei Munteanu <matei.munteanu@mta.ro>

--- src/Size.java
+++ src/Size.java
@@ -3,13 +3,10 @@ import java.nio.file.Files;
 import java.util.*;
 
 public class Size {
-
-    private int minim ;
     private int maxim;
     private String url;
 
     public Size(int minim, int maxim, String url) {
-        this.minim = minim;
         this.maxim = maxim;
         this.url = url;
     }
@@ -18,9 +15,9 @@ public class Size {
     {
         File files = new File(url);
         String[] fisiere = files.list();
-        for (String x : fisiere)
-            if (x.length() < maxim && x.length() > minim)
-                System.out.println("Fisierul " + x.toString() + " are lungimea de " + x.length() + "MB");
+        for (String fis : fisiere)
+            if (fis.length() < maxim)
+                System.out.println("Fisierul " + x.toString() + " are lungimea de " + fis.length() + "MB");
 
     }
 }
