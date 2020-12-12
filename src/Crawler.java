feff38ebf2cd69ff50799f74db4ac4f2c908ba0c
Catalin Raceanu <catalin.raceanu@mta.ro>
2020-12-12 19:46:52 +0200

    Update Main
    Issue: #8
    Am imbunatatit clasa main
    si am creat un executabil .jar
    
    Signed-off-by: Catalin Raceanu <catalin.raceanu@mta.ro>

--- src/Crawler.java
+++ src/Crawler.java
@@ -1,6 +1,9 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Vector;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
 
 public class Crawler {
 
@@ -8,8 +11,8 @@ public class Crawler {
         System.out.println("Alegeti una dintre comenzile disponibile:");
         System.out.println("1. Descarcare pagini: crawler crawl <config_file>");
         System.out.println("2. Cautare cuvant cheie: crawler search <key_word> <config_file>");
-        System.out.println("3. Filtrare documente: crawler filter type <file_type>");
-        System.out.println("                       crawler filter size <max_size>");
+        System.out.println("3. Filtrare documente: crawler filter type <file_type> <config_file>");
+        System.out.println("                       crawler filter size <max_size> <config_file>");
         System.out.println("4. Creare sitemap: crawler sitemap");
 
         try {
@@ -19,6 +22,8 @@ public class Crawler {
             Integer P = CM.get_ThreadsNumber_();
             Integer log = CM.get_LogLevel_();
             String store = CM.get_Rootdir_();
+            String store2 = store.substring(0, store.length() - 1);
+
 
 
 
@@ -27,7 +32,7 @@ public class Crawler {
 
                 for(int i=0;i<P;i++)
                 {
-                    MyThread Th = new MyThread("Thread", i, v, log, store, P);
+                    MyThread Th = new MyThread("Thread", i, v, log, store2, P);
                     Th.start();
                 }
 
@@ -35,27 +40,41 @@ public class Crawler {
             }
 
 
+
             else if (args[0].equals("search"))
             {
                 Search src = new Search();
-                src.search(args[1], args[2]);
+                src.search(args[1], store2);
 
             }
+
+
 
 
             else if (args[0].equals("filter"))
             {
                 if (args[1].equals("type"))
                 {
-
+                    FilterType filt1 = new FilterType(store2, args[2]);
+                    filt1.run();
+                    filt1.Printare();
                 }
 
                 if (args[1].equals("size"))
                 {
+                    Size filt2= new Size(0, Integer.parseInt(args[2]),store);
+                    filt2.run();
 
                 }
 
             }
+
+            else if (args[0].equals("sitemap"))
+            {
+
+            }
+
+
 
 
 
