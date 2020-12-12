598e28d16babde21adca0aaf9ade1f91bf8da569
Matei Munteanu <matei.munteanu@mta.ro>
2020-12-12 23:47:50 +0200

    Modificare clasa FilterByType
    Issue:#6
    Am modificat acea clasa astfel incat sa mearga in adancime.
    
    Signed-off-by: Matei Munteanu <matei.munteanu@mta.ro>

--- src/FilterByType.java
+++ src/FilterByType.java
@@ -2,29 +2,51 @@
 import java.io.File;
 import java.io.FilenameFilter;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Vector;
 
 public class FilterByType {
     private String extensie;
-    private String urluri;
-    private String[] fisiere;
+    private String root;
+    private List<String> fisiere;
 
     public FilterByType(String url, String extensie) {
-        this.urluri = url;
+        this.root = url;
         this.extensie = extensie;
     }
 
 
     public void run(){
-        File files = new File(urluri);
-        fisiere = files.list(new FilenameFilter() {
-            @Override
-            public boolean accept(File dir, String name) {
-                return name.endsWith(extensie);
+        File directory = new File(root);
+
+        File[] fList = directory.listFiles();
+        if (fList!=null)
+        {
+            for(File fisier : fList)
+            {
+                if(fisier.isFile())
+                {
+                    Path path = Paths.get(fisier.toString());
+                    String name = path.getFileName().toString();
+                    int index = name.lastIndexOf('.');
+                    if (index > 0 )
+                    {
+                        String ext = name.substring(index+1);
+                        if (ext.equals(extensie))
+                            fisiere.add(name);
+                    }
+                }
+                else if (fisier.isDirectory())
+                {
+                    this.root = fisier.toString();
+                    run();
+                }
             }
-        });
+        }
     }
-    public void Printare (){
+    public void Printare(){
         for (String s : fisiere)
             System.out.println(s);
     }
