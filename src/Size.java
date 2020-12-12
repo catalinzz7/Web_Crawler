ba1832ed65c6b355713d3d4e93c1fa6b157f27b7
Matei Munteanu <matei.munteanu@mta.ro>
2020-12-13 00:08:42 +0200

    Modificare clasa Size
    Issue:#7
    Am modificat clasa Size astfel incat sa afiseze sub o forma
    arborescenta.
    
    Signed-off-by: Matei Munteanu <matei.munteanu@mta.ro>

--- src/Size.java
+++ src/Size.java
@@ -1,23 +1,45 @@
 import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class Size {
     private int maxim;
-    private String url;
+    private String root;
+    private List<String> fisiere;
 
-    public Size(int minim, int maxim, String url) {
+    public Size(int minim, int maxim, String root) {
         this.maxim = maxim;
-        this.url = url;
+        this.root = root;
     }
 
     public void run()
     {
-        File files = new File(url);
-        String[] fisiere = files.list();
-        for (String fis : fisiere)
-            if (fis.length() < maxim)
-                System.out.println("Fisierul " + x.toString() + " are lungimea de " + fis.length() + "MB");
+        File directory = new File(root);
 
+        File[] fList = directory.listFiles();
+        if (fList!=null)
+        {
+            for(File fisier : fList)
+            {
+                if(fisier.isFile())
+                {
+                    if (fisier.length() < maxim)
+                        fisiere.add(fisier.toString());
+                }
+                else if (fisier.isDirectory())
+                {
+                    this.root = fisier.toString();
+                    run();
+                }
+            }
+        }
+    }
+
+    public void Printare()
+    {
+        for (String s : fisiere)
+            System.out.println(s);
     }
 }
