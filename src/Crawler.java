459846cee80bada5aa954788c006ba46616a969b
Catalin Raceanu <catalin.raceanu@mta.ro>
2020-12-12 17:49:18 +0200

    Update Main
    Issue: #8
    
    Am modificat clasa Crawler.java
    pentru a utiliza argumente la apel
    
    Signed-off-by: Catalin Raceanu <catalin.raceanu@mta.ro>

--- src/Crawler.java
+++ src/Crawler.java
@@ -5,7 +5,12 @@ import java.util.Vector;
 public class Crawler {
 
     public static void main(String[] args) {
-
+        System.out.println("Alegeti una dintre comenzile disponibile:");
+        System.out.println("1. Descarcare pagini: crawler crawl <config_file>");
+        System.out.println("2. Cautare cuvant cheie: crawler search <key_word> <config_file>");
+        System.out.println("3. Filtrare documente: crawler filter type <file_type>");
+        System.out.println("                       crawler filter size <max_size>");
+        System.out.println("4. Creare sitemap: crawler sitemap");
 
         try {
             CrawlerManager CM = new CrawlerManager("src/data/config.cnf", "src/data/URLs.txt");
@@ -15,30 +20,42 @@ public class Crawler {
             Integer log = CM.get_LogLevel_();
             String store = CM.get_Rootdir_();
 
-            //if(args[0].equals("crawl"))
-           // {
-                //System.out.println("Hello World!");
 
-            for(int i=0;i<P;i++)
+
+            if(args[0].equals("crawl"))
             {
-                MyThread Th = new MyThread("Thread", i, v, log, store, P);
-                Th.start();
+
+                for(int i=0;i<P;i++)
+                {
+                    MyThread Th = new MyThread("Thread", i, v, log, store, P);
+                    Th.start();
+                }
+
+
             }
 
 
-           // }
+            else if (args[0].equals("search"))
+            {
+                Search src = new Search();
+                src.search(args[1], args[2]);
 
-            //else if (args[0].equals("search"))
-           // {
-
-
-           // }
-
-           // else if (args[0].equals("filter"))
-          //  {
+            }
 
 
-            //}
+            else if (args[0].equals("filter"))
+            {
+                if (args[1].equals("type"))
+                {
+
+                }
+
+                if (args[1].equals("size"))
+                {
+
+                }
+
+            }
 
 
 
