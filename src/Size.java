4ba7da51d7028ffbc2439c536b09bdf1fd2a01a3
Matei Munteanu <matei.munteanu@mta.ro>
2020-12-15 10:08:54 +0200

    Modificare clasa size
    Issue:#35
    Am modificat clasa size.java, deoarece nu mergea recursiv.
    Sper sa mearga ok acum, orice problema, call bby.
    
    Signed-off-by: Matei Munteanu <matei.munteanu@mta.ro>

--- src/Size.java
+++ src/Size.java
@@ -1,23 +1,69 @@
 import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
-public class Size {
-    private int maxim;
-    private String url;
+/**
+ * Creeare clasa Size.java, care va filtra fisierele descarcate
+ * dupa marimea acestora. Pentru a utiliza aceasta clasa, trebuie
+ * creeat un obiect folosind constructorul explicit, apoi apelate
+ * functiile run si Printare.
+ *
+ * @author MateiMunteanu
+ */
 
-    public Size(int minim, int maxim, String url) {
+public class Size {
+    /**
+     * Initializeaza elementele primite din main.
+     */
+    private int maxim;
+    private String root;
+    private List<String> fisiere;
+
+    /**
+     *
+     * @param maxim - nr maxim de MB pe care sa il aiba fisierul.
+     * @param root_dir - fisierul root_dir din functia main (unde se descarca fisierele)
+     */
+    public Size(int maxim, String root_dir) {
         this.maxim = maxim;
-        this.url = url;
+        this.root = root_dir;
+        this.fisiere = new ArrayList<String>();
     }
 
+    /**
+     * Rularea propriu-zisa a aplicatiei.
+     */
     public void run()
     {
-        File files = new File(url);
-        String[] fisiere = files.list();
-        for (String fis : fisiere)
-            if (fis.length() < maxim)
-                System.out.println("Fisierul " + x.toString() + " are lungimea de " + fis.length() + "MB");
+        try {
+            File directory = new File(root);
+            File[] fList = directory.listFiles();
+            if (fList != null) {
+                for (File fisier : fList) {
+                    if (fisier.isFile()) {
+                       if (fisier.length() < maxim)
+                            fisiere.add(fisier.toString());
+                    } else if (fisier.isDirectory()) {
+                        this.root = fisier.toString();
+                        run();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Nu am putut rula executia filtrarii.\n");
 
+        }
+
+    }
+
+    /**
+     * Printarea elementelor care au marimea mai mica de X MB
+     */
+    private void Printare()
+    {
+        for (String s : fisiere)
+            System.out.println(s);
     }
 }
