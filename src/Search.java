d73525278c52a0e7ca03f63643f48e51dd2b833c
alexandrudan1 <allexdan98@gmail.com>
2020-12-15 20:48:39 +0200

    Testare Search.java
    Issue: #35
    Am testat Search.java si am remediat eventualele erori aparute.
    
    Signed-off-by: alexandrudan1 <allexdan98@gmail.com>

--- src/Search.java
+++ src/Search.java
@@ -8,95 +8,92 @@ import java.util.*;
  * @author Dan Alexandru
  */
 
-public class Search
-{
-    /*
-    mDir reprezinta root_dir-ul
-    urls contine URL-urile in care apare cuvantul cheie si numarul de aparitii
-    mKey reprezinta cuvantul cheie
+public class Search {
+    /**
+     * mDir reprezinta root_dir-ul
+     * urls contine URL-urile in care apare cuvantul cheie si numarul de aparitii
+     * mKey reprezinta cuvantul cheie
      */
 
     File mDir;
     Map<Integer, String> urls = new HashMap<Integer, String>();
     String mKey;
 
-    /*
-    constructorul
+    /**
+     * constructorul
      */
-    public Search()
-    {
+    public Search() {
     }
 
-    /*
-    functia de verificare a existentei root_dir
+    /**
+     * functia de verificare a existentei root_dir
      */
-    public void search(String key,String dir)
-    {
+    public void search(String key, String dir) {
         this.mDir = new File(dir);
         this.mKey = key;
-        if(mDir.exists())
-        {
-            searchInTextFiles(mKey,mDir);
+        if (mDir.exists()) {
+            searchInTextFiles(mKey, mDir);
+            Log.getInstance().writeToFile(" INFO: Caut cuvantul " + mKey + " in " + mDir + "!\n");
             print();
-        }
-        else
+        } else {
             System.out.println("Nu exista fisierul!");
+            Log.getInstance().writeToFile("Warning : Fisierul " + mDir + " nu a fost gasit" + "!\n");
+        }
+
     }
 
-    /*
-    Sortare URL-uri descrescator dupa frecventa de aparitie a cuvantului cheie si afisarea acestora
+    /**
+     * Sortare URL-uri descrescator dupa frecventa de aparitie a cuvantului cheie si afisarea acestora
      */
-    public void print()
-    {
-        TreeMap< Integer, String> sorted = new TreeMap<>(Collections.reverseOrder());
+    public void print() {
+        TreeMap<Integer, String> sorted = new TreeMap<>(Collections.reverseOrder());
         sorted.putAll(urls);
-        for (Map.Entry<Integer, String> entry : sorted.entrySet())
-            System.out.println(entry.getValue());
-    }
-
-    /*
-    Functia de cautare recursiva in root_dir
-     */
-    public void searchInTextFiles(String mKey,File dir)
-    {
-        File[] a = dir.listFiles();
-        for (File f : a)
-        {
-            if (f.isDirectory())
-                searchInTextFiles(mKey,f);
-            else
-                searchInFile(mKey,f);
+        int i = 1;
+        System.out.println("\nCautarea  cheii " + mKey + ", dupa relevanta in fisierul " + mDir +
+                " are urmatoarele rezultate:");
+        for (Map.Entry<Integer, String> entry : sorted.entrySet()) {
+            System.out.println(i + ". " + entry.getValue());
+            i++;
         }
     }
 
-    /*
-    Functia de cautare a cuvantului cheie si numararea aparitiei acestuia in fiecare URL
-    */
-    private void searchInFile(String key,File f)
-    {
+    /**
+     * Functia de cautare recursiva in root_dir
+     */
+    public void searchInTextFiles(String mKey, File dir) {
+        File[] a = dir.listFiles();
+        for (File f : a) {
+            if (f.isDirectory())
+                searchInTextFiles(mKey, f);
+            else
+                searchInFile(mKey, f);
+        }
+    }
+
+    /**
+     * Functia de cautare a cuvantului cheie si numararea aparitiei acestuia in fiecare URL
+     */
+    private void searchInFile(String key, File f) {
         int count = 0;
-        try
-        {
+        try {
             FileInputStream fstream = new FileInputStream(f);
             DataInputStream in = new DataInputStream(fstream);
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             String strLine;
-            while ((strLine = br.readLine()) != null)
-            {
+            while ((strLine = br.readLine()) != null) {
                 int startIndex = strLine.indexOf(key);
-                while (startIndex != -1)
-                {
+                while (startIndex != -1) {
                     count++;
                     startIndex = strLine.indexOf(key,
-                            startIndex +key.length());
+                            startIndex + key.length());
                 }
             }
             in.close();
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             System.err.println("Eroare: " + e.getMessage());
+            Log.getInstance().writeToFile("Warning : " + e.getMessage() + "!\n");
         }
-        if(count>0)
-        urls.put(new Integer(count),f.toString()); //inserarea URL-ului in map
+        if (count > 0)
+            urls.put(new Integer(count), f.toString()); //inserarea URL-ului in map
     }
 }
